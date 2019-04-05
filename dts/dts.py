900d9f3c4f7d91ad8f232fe10b0d548cf4ecf98e
martineliasq <elias.quintero@udea.edu.co>
2019-04-05 08:49:05 -0500

    add pre-model funtion

--- dts/dts.py
+++ dts/dts.py
@@ -1,4 +1,4 @@
-from dts import load_image
+#from dts import load_images
 import tensorflow as tf
 from tensorflow import keras
 
@@ -9,4 +9,21 @@ class dts(object):
     """
 
     def __init__(self, *args):
+        pass
+
+    @staticmethod
+    def _get_vgg19():
+        """Get VGG model and weights with ImageNet"""
+        vgg19 = keras.applications.vgg19.VGG19(
+            weights='imagenet',  include_top=False)
+        return vgg19
+
+    @staticmethod
+    def _freeze_layers(model):
+        for layer in model.layers:
+            layer.trainable = False
+        return model
+
+    @staticmethod
+    def _get_layers(model):
         pass
