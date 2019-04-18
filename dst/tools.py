1aa9d5b187126e57845372ad3eef4eaae2b1b70e
martineliasq <elias.quintero@udea.edu.co>
2019-04-18 12:10:52 -0500

    Add funtions to process image- Documentation.

--- dst/tools.py
+++ dst/tools.py
@@ -5,33 +5,77 @@ from tensorflow import keras
 from PIL import Image
 
 
-def _open_image(path_image: str = ""):
-    """ Load image
-        path_image: Path of the image
-        return-> numpy array with the image"""
-    return Image.open(path_image)
+def _open_image(image_path: str = None):
+    ''' Load the image that is in the  image_path
+
+        Args:
+             path_image(str): Source address where the image is located
+
+        Returns:
+                image: Image
+    '''
+    assert(image_path is not None), "You should provide an image path"
+    return Image.open(image_path)
 
 
 def _image_to_array(image):
+    ''' Convert image object to numpy array(generally a matrix with 3 dimensions)
+
+        Args:
+             image (Image object): image to convert to matrix
+        Returns:
+                image: Matrix with the values of the image.(numpy.ndarray)
+    '''
+
     return np.array(image)
 
 
 def _add_dim(tensor):
+    ''' Add dimensions of a tensor in the axis 0.
+
+        Args:
+             tensor (numpy.ndarray): Tensor
+        Returns:
+                tensor: Tensor with one more dimension
+    '''
+    assert(type(tensor) is np.ndarray), "Tensor should be numpy.ndarray"
     return np.expand_dims(tensor, axis=0)
 
 
 def _del_dim(tensor):
+    ''' Delete dimensions of a tensor in the axis 0.
+
+        Args:
+             tensor (numpy.ndarray): Tensor
+        Returns:
+                dims: Tensor.
+    '''
+    assert(type(tensor) is np.ndarray), "Tensor should be numpy.ndarray"
     return np.squeeze(tensor, axis=0)
 
 
 def _scale_image(image, max_dim=512):
-    # ANTIALIAS -> (a high-quality downsampling filter).
+    ''' The image is scaled relative to the maximum dimension
+
+        Args:
+             image (Image Object): Image
+             max_dim: Maximum dimension of the image
+        Returns:
+                Image Object: A image resize with the maxium dimension
+    '''
+
     scale = max_dim / max(image.size)
+
+    #  Image.ANTIALIAS : A Anti-aliasing  is applied
+    #                    to the image to make it look better.
+    #  ANTIALIAS -> (a high-quality downsampling filter).
+
     return image.resize((round(image.size[0]*scale),
                          round(image.size[1]*scale)), Image.ANTIALIAS)
 
 
 def get_image_dim(image):
+    ''''''
     return image.shape
 
 
@@ -43,11 +87,18 @@ def _load_image(path_image, max_dim=512):
     return img.astype('uint8')
 
 
-def imshow(image, title=None):
-    # Remove the batch dimension
-    out = np.squeeze(image, axis=0)
+def imshow(img, title=None):
+    ''' Show image and title.
+        Args:
+             img: Image to Show
+             title: Title Head of image
+    '''
+
+    if len(img.shape) == 4:
+        # Remove the batch dimension
+        img = np.squeeze(img, axis=0)
     # Normalize for display
-    out = out.astype('uint8')
+    out = img.astype('uint8')
     plt.imshow(out)
     if title is not None:
         plt.title(title)
@@ -55,8 +106,64 @@ def imshow(image, title=None):
 
 
 def _load_and_process_img(path_image, max_dim=512):
+    ''' Load image(open, scale, convert in numpy array and add batch dim to
+         image) and preprocess image in the VGG format. (Color format BGR and 
+         normalize pixels with the mean)
+
+        Arg:
+            path_image(str): Source address where the image is located
+            max_dim: Maximun dimension of the image(Scale Relative)
+        Returns:
+                image preprocess(VGG format)
+    '''
     img = _load_image(path_image, max_dim)
 
     # Preprocess input image to vgg requierements
     # That is a normalize(central) pixel of image
-    return keras.applications.vgg19.preprocess_input(img)
+    img = keras.applications.vgg19.preprocess_input(img)
+    return img
+
+
+def deprocess_img(processed_img, color_format="rgb"):
+    ''' Convert a processed image (VGG Format) to normal image(RGB and Non
+        Normalize pixels)
+
+        Arg:
+            processed_img: Matrix or Tensor with the values of the retult image
+            color_format: rgb or bgr format to the image
+        Returns:
+                Matrix with the pixels without normalize (optional : RGB)
+                [If you don't pass rgb arguments, the channels will be BGR ]
+    '''
+    x = processed_img.copy()
+
+    if len(x.shape) == 4:
+        # Remove the batch dimension
+        x = np.squeeze(x, 0)
+    assert len(x.shape) == 3, ("Input to deprocess image must be an image of "
+                               "dimension [1, height, width, channel] or"
+                               "[height, width, channel]")
+    if len(x.shape) != 3:
+        raise ValueError("Invalid input to deprocessing image")
+
+    # perform the inverse of the preprocessiing step
+    # That is a inverse step that process image
+    # That values are  mean pixel values(Normalize)
+    x[:, :, 0] += 103.939
+    x[:, :, 1] += 116.779
+    x[:, :, 2] += 123.68
+
+    # Convert image BGR to RGB - invert colors
+    # The data has 3 dimensions: width, height, and color.
+    #  ::-1 effectively reverses the order of the colors.
+    # The width and height are not affected.
+    # Source:
+    # https://stackoverflow.com/questions/4661557/pil-rotate-image-colors-bgr-rgb
+    if color_format == 'rgb':
+        x = x[:, :, ::-1]
+    # Clip Values between 0 and 255 (Pixel Values)
+    x = np.clip(x, 0, 255).astype('uint8')
+    return x
+
+
+img = _load_and_process_img("../images/perfil.jpg")
