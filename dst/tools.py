355f6ef09b9b6599c44b8ddace94ce81473dad4b
martineliasq <elias.quintero@udea.edu.co>
2019-04-17 23:53:02 -0500

    Load Image Done!

--- dst/tools.py
+++ dst/tools.py
@@ -1,15 +1,42 @@
 """Generic tools"""
 import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
 
 
-def _load_image(path_image: str = ""):
+def _open_image(path_image: str = ""):
     """ Load image
         path_image: Path of the image
         return-> numpy array with the image"""
-    return plt.imread(path_image)
+    return Image.open(path_image)
 
 
-def load_images(path_image1, path_image2):
-    """Load a couple image and return tuple with
-        two numpy arrays(each image)"""
-    return (_load_image(path_image1), _load_image(path_image2))
+def _image_to_array(image):
+    return np.array(image)
+
+
+def _add_dim(tensor):
+    return np.expand_dims(tensor, axis=0)
+
+
+def _del_dim(tensor):
+    return np.squeeze(tensor, axis=0)
+
+
+def _scale_image(image, max_dim=512):
+    # ANTIALIAS -> (a high-quality downsampling filter).
+    scale = max_dim / max(image.size)
+    return image.resize((round(image.size[0]*scale),
+                         round(image.size[1]*scale)), Image.ANTIALIAS)
+
+
+def get_image_dim(image):
+    return image.shape
+
+
+def _load_image(path_image, max_dim=512):
+    img = _open_image(path_image)
+    img = _scale_image(img, max_dim)
+    img = _image_to_array(img)
+    img = _add_dim(img)
+    return img.astype('uint8')
