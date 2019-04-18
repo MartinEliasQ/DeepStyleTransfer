500799395be725b42f5dfa4b291e301b01078e4e
martineliasq <elias.quintero@udea.edu.co>
2019-04-18 00:04:33 -0500

    Add Keras(tf api), Add imshow funtion

--- dst/tools.py
+++ dst/tools.py
@@ -1,6 +1,7 @@
 """Generic tools"""
 import matplotlib.pyplot as plt
 import numpy as np
+from tensorflow import keras
 from PIL import Image
 
 
@@ -40,3 +41,22 @@ def _load_image(path_image, max_dim=512):
     img = _image_to_array(img)
     img = _add_dim(img)
     return img.astype('uint8')
+
+
+def imshow(image, title=None):
+    # Remove the batch dimension
+    out = np.squeeze(image, axis=0)
+    # Normalize for display
+    out = out.astype('uint8')
+    plt.imshow(out)
+    if title is not None:
+        plt.title(title)
+        plt.imshow(out)
+
+
+def _load_and_process_img(path_image, max_dim=512):
+    img = _load_image(path_image, max_dim)
+
+    # Preprocess input image to vgg requierements
+    # That is a normalize(central) pixel of image
+    return keras.applications.vgg19.preprocess_input(img)
